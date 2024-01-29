f2fcb7db1efe03b43906888f9dd05876097ad9c4
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-29 20:20:50 +0000

    Add Counter component with increment, decrement, and reset functionality

---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,40 @@
+import { decrement, increment, incrementByAmount, reset } from './counterSlice'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { useState } from 'react'
+
+const Counter = () => {
+    const count = useSelector((state) => state.counter.count)
+    const dispatch = useDispatch()
+    const [incrementAmount, setIncrementAmount] = useState(0)
+    const addValue = Number(incrementAmount) || 0
+
+    const resetAll = () => {
+        setIncrementAmount(0)
+        dispatch(reset())
+    }
+
+    return (
+        <section>
+            <p>{count}</p>
+            <div>
+                <button onClick={() => dispatch(increment())}>+</button>
+                <button onClick={() => dispatch(decrement())}>-</button>
+            </div>
+            <input
+                type="text"
+                value={incrementAmount}
+                onChange={(e) => setIncrementAmount(e.target.value)}
+            />
+
+            <div>
+                <button onClick={() => dispatch(incrementByAmount(addValue))}>
+                    Add amount
+                </button>
+                <button onClick={resetAll}>Reset</button>
+            </div>
+        </section>
+    )
+}
+
+export default Counter
