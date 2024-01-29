7ba5d3cd2e2623d4e72eb2e7e38d07dcaec4cd84
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-29 20:48:45 +0000

    Add PostList component

---
+++ src/features/post/PostList.jsx
@@ -0,0 +1,24 @@
+import { selectAllPosts } from './postsSlice'
+import { useSelector } from 'react-redux'
+
+const PostList = () => {
+    const posts = useSelector(selectAllPosts)
+
+    const renderedPosts = posts.map((post) => {
+        return (
+            <article className="post" key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.content.substring(0, 100)}</p>
+            </article>
+        )
+    })
+
+    return (
+        <section className="posts-list">
+            <h2>Posts</h2>
+            {renderedPosts}
+        </section>
+    )
+}
+
+export default PostList
