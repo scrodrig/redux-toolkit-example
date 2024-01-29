2b3d9e8afe2702378d157fd1e154862c41a70432
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-29 20:48:53 +0000

    Add PostList component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import Counter from './features/counter/Counter' //* removed from lesson01
+import PostList from './features/post/PostList'
 
 function App() {
     return (
         <main className="App">
             {/* ! removed from lesson01, <Counter /> */}
+            <PostList />
         </main>
     )
 }
