5883022ec6b31d34f825574ef4d28f57afff2d89
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-29 20:39:53 +0000

    Add postsSlice.js file

---
+++ src/features/post/postsSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+    {
+        id: '1',
+        title: 'First Post!',
+        content: 'A long time ago in a galaxy far, far away...',
+    },
+    {
+        id: '2',
+        title: 'Second Post',
+        content: 'A long time ago in a galaxy far, far away...',
+    },
+]
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {},
+})
+
+export default postsSlice.reducer
