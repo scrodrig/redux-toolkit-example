1b062ac25da9c4bbfc0acc6430025cd03de661ff
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-29 20:40:01 +0000

    Remove counterReducer and add postReducer to store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
+// import counterReducer from '../features/counter/counterSlice' //! remove from lesson01
+import postReducer from '../features/post/postsSlice'
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
+        // counter: counterReducer, //! This is the original line, for testing
+        posts: postReducer,
     },
 })
