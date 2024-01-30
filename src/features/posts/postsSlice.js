f0475983931d6045f457cfaba929d7a28f319365
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-30 22:17:28 +0000

    Add async thunks for fetching and adding posts

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,53 +1,42 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, nanoid } from '@reduxjs/toolkit'
 
+import axios from 'axios'
 import { sub } from 'date-fns'
 
-const initialState = [
-    {
-        id: '1',
-        title: 'First Post!',
-        content: 'A long time ago in a galaxy far, far away...',
-        date: sub(new Date(), { minutes: 10 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0,
-        },
-    },
-    {
-        id: '2',
-        title: 'Second Post',
-        content: 'A long time ago in a galaxy far, far away...',
-        date: sub(new Date(), { minutes: 5 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0,
-        },
-    },
-]
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
+
+const initialState = {
+    posts: [],
+    status: 'idle', //'idle' | 'loading' | 'succeeded' | 'failed'
+    error: null,
+}
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const response = await axios.get(POSTS_URL)
+    return response.data
+})
+
+export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPost) => {
+    const response = await axios.post(POSTS_URL, initialPost)
+    return response.data
+})
 
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
         postAdded: {
-            reducer: (state, action) => {
-                state.push(action.payload)
+            reducer(state, action) {
+                state.posts.push(action.payload)
             },
-            //* prepare callback is used to modify the action object before it is passed to the reducer
-            prepare: (title, content, userId) => {
+            prepare(title, content, userId) {
                 return {
                     payload: {
                         id: nanoid(),
                         title,
                         content,
-                        userId,
                         date: new Date().toISOString(),
+                        userId,
                         reactions: {
                             thumbsUp: 0,
                             wow: 0,
@@ -59,18 +48,72 @@ const postsSlice = createSlice({
                 }
             },
         },
-        reactionAdded: (state, action) => {
+        reactionAdded(state, action) {
             const { postId, reaction } = action.payload
-            const existingPost = state.find((post) => post.id === postId)
+            const existingPost = state.posts.find((post) => post.id === postId)
             if (existingPost) {
                 existingPost.reactions[reaction]++
             }
         },
     },
+    extraReducers(builder) {
+        builder
+            .addCase(fetchPosts.pending, (state, action) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                // Adding date and reactions
+                let min = 1
+                const loadedPosts = action.payload.map((post) => {
+                    post.date = sub(new Date(), { minutes: min++ }).toISOString()
+                    post.reactions = {
+                        thumbsUp: 0,
+                        wow: 0,
+                        heart: 0,
+                        rocket: 0,
+                        coffee: 0,
+                    }
+                    return post
+                })
+
+                // Add any fetched posts to the array
+                state.posts = state.posts.concat(loadedPosts)
+            })
+            .addCase(fetchPosts.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message
+            })
+            .addCase(addNewPost.fulfilled, (state, action) => {
+                /* ! Fix for API post IDs: Creating sortedPosts & assigning the 
+                id would be not be needed if the fake API returned accurate new post IDs */
+                const sortedPosts = state.posts.sort((a, b) => {
+                    if (a.id > b.id) return 1
+                    if (a.id < b.id) return -1
+                    return 0
+                })
+                action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1
+                //! End fix for fake API post IDs
+
+                action.payload.userId = Number(action.payload.userId)
+                action.payload.date = new Date().toISOString()
+                action.payload.reactions = {
+                    thumbsUp: 0,
+                    hooray: 0,
+                    heart: 0,
+                    rocket: 0,
+                    eyes: 0,
+                }
+                console.log(action.payload)
+                state.posts.push(action.payload)
+            })
+    },
 })
 
-export const selectAllPosts = (state) => state.posts
+export const selectAllPosts = (state) => state.posts.posts
+export const getPostStatus = (state) => state.posts.status
+export const getPostError = (state) => state.posts.error
 
-export const { postAdded, reactionAdded } = postsSlice.actions //* actions are automatically generated by createSlice with the same name of reducers object
+export const { postAdded, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
