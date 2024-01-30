6db69d19d17e9fe9aeaa2dc52f19d8ec2feb51f8
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-30 22:17:23 +0000

    Add async thunk for fetching users from API

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,16 +1,30 @@
 // cSpell:ignore reduxjs
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
-const initialState = [
-    { id: '0', name: 'Obi wan Kenobi' },
-    { id: '1', name: 'Luke Skywalker' },
-    { id: '2', name: 'Yoda' },
-]
+import axios from 'axios'
+
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
+
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    try {
+        const response = await axios.get(USERS_URL)
+        return [...response.data]
+    } catch (err) {
+        return err.message
+    }
+})
 
 const usersSlice = createSlice({
     name: 'users',
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            return action.payload
+        })
+    },
 })
 
 export const selectAllUsers = (state) => state.users
