9d5817912b95165cea23037e972346d4f25e8ea3
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-30 19:26:25 +0000

    Add AddPostForm component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import AddPostForm from './features/post/AddPostForm'
 // import Counter from './features/counter/Counter' //* removed from lesson01
 import PostList from './features/post/PostList'
 
@@ -5,6 +6,7 @@ function App() {
     return (
         <main className="App">
             {/* ! removed from lesson01, <Counter /> */}
+            <AddPostForm />
             <PostList />
         </main>
     )
