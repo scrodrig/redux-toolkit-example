12dc67caf7da406373641d13b3879c263bfdd225
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-30 20:24:34 +0000

    Add PostAuthor component

---
+++ src/features/posts/PostAuthor.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+import { selectAllUsers } from '../users/usersSlice'
+import { useSelector } from 'react-redux'
+
+const PostAuthor = ({ userId }) => {
+    const users = useSelector(selectAllUsers)
+
+    const author = users.find((user) => user.id === userId)
+
+    return <span>by {author ? author.name : 'Unknown author'}</span>
+}
+
+PostAuthor.propTypes = {
+    userId: PropTypes.string,
+}
+
+export default PostAuthor
