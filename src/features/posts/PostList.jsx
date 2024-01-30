9d45bf2e814d382cd57d75d210be19e8987f3508
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-30 22:17:39 +0000

    Refactor PostList component to use new Redux selectors and dispatch actions
    This commit refactors the PostList component in src/features/posts/PostList.jsx to use the new Redux selectors and dispatch actions from the postsSlice. It replaces the import statements for PostAuthor, ReactionButtons, and TimeAgo with the new imports for fetchPosts, getPostError, getPostStatus, selectAllPosts, useDispatch, and useSelector. It also adds a useEffect hook to dispatch the fetchPosts action when the postStatus is 'idle'. The renderedPosts variable is replaced with the content variable, which is conditionally rendered based on the postStatus. The commit also updates the component name from PostList to PostsList.

--- src/features/posts/PostList.jsx
+++ src/features/posts/PostList.jsx
@@ -1,36 +1,37 @@
-import PostAuthor from './PostAuthor'
-import ReactionButtons from './ReactionButtons'
-import TimeAgo from './TimeAgo'
-import { selectAllPosts } from './postsSlice'
-import { useSelector } from 'react-redux'
+import { fetchPosts, getPostError, getPostStatus, selectAllPosts } from './postsSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
-const PostList = () => {
+import PostExcerpt from './PostExcerpt'
+import { useEffect } from 'react'
+
+const PostsList = () => {
+    const dispatch = useDispatch()
+
     const posts = useSelector(selectAllPosts)
+    const postStatus = useSelector(getPostStatus)
+    const error = useSelector(getPostError)
 
-    const orderedPosts = posts
-        .slice()
-        .sort((a, b) => b.date.localeCompare(a.date))
+    useEffect(() => {
+        if (postStatus === 'idle') {
+            dispatch(fetchPosts())
+        }
+    }, [postStatus, dispatch])
 
-    const renderedPosts = orderedPosts.map((post) => {
-        return (
-            <article className="post" key={post.id}>
-                <h3>{post.title}</h3>
-                <p>{post.content.substring(0, 100)}</p>
-                <p className="postCredit">
-                    <PostAuthor userId={post.userId} />
-                    <TimeAgo timestamp={post.date} />
-                </p>
-                <ReactionButtons post={post} />
-            </article>
-        )
-    })
+    let content
+    if (postStatus === 'loading') {
+        content = <p>Loading...</p>
+    } else if (postStatus === 'succeeded') {
+        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+        content = orderedPosts.map((post) => <PostExcerpt key={post.id} post={post} />)
+    } else if (postStatus === 'failed') {
+        content = <p>{error}</p>
+    }
 
     return (
-        <section className="posts-list">
+        <section>
             <h2>Posts</h2>
-            {renderedPosts}
+            {content}
         </section>
     )
 }
-
-export default PostList
+export default PostsList
