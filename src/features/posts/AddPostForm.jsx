39f0df77fbf4177414671429924fb5fcad97efa4
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-30 22:17:48 +0000

    Refactor AddPostForm component and update save functionality

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -1,13 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux'
 
-import { postAdded } from './postsSlice'
+import { addNewPost } from './postsSlice'
 import { selectAllUsers } from '../users/usersSlice'
+import { set } from 'date-fns'
 import { useState } from 'react'
 
 const AddPostForm = () => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
     const [userId, setUserId] = useState('')
+    const [addTequestStatus, setAddRequestStatus] = useState('idle')
+
     const users = useSelector(selectAllUsers)
 
     const onTitleChanged = (e) => setTitle(e.target.value)
@@ -16,16 +19,25 @@ const AddPostForm = () => {
 
     const dispatch = useDispatch()
 
+    // const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+    const canSave = [title, content, userId].every(Boolean) && addTequestStatus === 'idle'
+
     const onSavePostClicked = () => {
-        if (title && content) {
-            dispatch(postAdded(title, content, userId))
-            setTitle('')
-            setContent('')
-            setUserId('')
+        if (canSave) {
+            try {
+                setAddRequestStatus('pending')
+                dispatch(addNewPost({ title, body: content, userId })).unwrap()
+                setTitle('')
+                setContent('')
+                setUserId('')
+            } catch (err) {
+                console.error('Failed to save the post: ', err)
+            } finally {
+                setAddRequestStatus('idle')
+            }
         }
     }
-
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
     const userOptions = users.map((user) => (
         <option key={user.id} value={user.id}>
@@ -38,34 +50,15 @@ const AddPostForm = () => {
             <h2>Add a New Post</h2>
             <form>
                 <label htmlFor="postTitle">Post Title:</label>
-                <input
-                    type="text"
-                    id="postTitle"
-                    name="postTitle"
-                    value={title}
-                    onChange={onTitleChanged}
-                />
+                <input type="text" id="postTitle" name="postTitle" value={title} onChange={onTitleChanged} />
                 <label htmlFor="postAuthor">Author:</label>
-                <select
-                    id="postAuthor"
-                    value={userId}
-                    onChange={onAuthorChanged}
-                >
+                <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
                     <option value=""></option>
                     {userOptions}
                 </select>
                 <label htmlFor="postContent">Content:</label>
-                <textarea
-                    id="postContent"
-                    name="postContent"
-                    value={content}
-                    onChange={onContentChanged}
-                />
-                <button
-                    disabled={!canSave}
-                    type="button"
-                    onClick={onSavePostClicked}
-                >
+                <textarea id="postContent" name="postContent" value={content} onChange={onContentChanged} />
+                <button disabled={!canSave} type="button" onClick={onSavePostClicked}>
                     Save Post
                 </button>
             </form>
