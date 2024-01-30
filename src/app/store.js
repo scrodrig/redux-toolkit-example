06438004d4aa89aa190f01a65dcd4fb983724abe
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-30 20:15:55 +0000

    Update store.js with new reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 // import counterReducer from '../features/counter/counterSlice' //! remove from lesson01
-import postReducer from '../features/post/postsSlice'
+import postsReducer from '../features/posts/postsSlice'
+import usersReducer from '../features/users/usersSlice'
 
 export const store = configureStore({
     reducer: {
         // counter: counterReducer, //! This is the original line, for testing
-        posts: postReducer,
+        posts: postsReducer,
+        users: usersReducer,
     },
 })
