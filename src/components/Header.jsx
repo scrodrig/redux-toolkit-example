4696258690dd7dd2e3e6e9289354ef321ed966e4
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-31 15:42:05 +0000

    Add count display and increase button to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
+import { getCount, increaseCount } from '../features/posts/postsSlice'
+import { useDispatch, useSelector } from 'react-redux'
+
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+    const dispatch = useDispatch()
+    const count = useSelector(getCount)
+
     return (
         <header className="Header">
             <h1>Redux Blog</h1>
@@ -12,7 +18,17 @@ const Header = () => {
                     <li>
                         <Link to="post">Post</Link>
                     </li>
+                    <li>
+                        <Link to="user">Users</Link>
+                    </li>
                 </ul>
+                <button
+                    onClick={() => {
+                        dispatch(increaseCount())
+                    }}
+                >
+                    {count}
+                </button>
             </nav>
         </header>
     )
