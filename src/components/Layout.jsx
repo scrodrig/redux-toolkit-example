8448ce6db5d3b3c1821e8520bcdaf65f54c6cba5
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-31 12:32:17 +0000

    Add Layout component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import Header from './Header'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+    return (
+        <>
+            <Header />
+            <main className="App">
+                <Outlet />
+            </main>
+        </>
+    )
+}
+
+export default Layout
