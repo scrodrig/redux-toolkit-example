0999796aa663d0a8e0b6a8bf35dc0db2f5f3a03a
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-31 17:00:24 +0000

    Add React.StrictMode to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 import App from './App.jsx'
 import { Provider } from 'react-redux'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { fetchPosts } from './features/posts/postsSlice.js'
 import { fetchUsers } from './features/users/usersSlice.js'
@@ -13,11 +14,13 @@ store.dispatch(fetchUsers())
 store.dispatch(fetchPosts())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-        <Router>
-            <Routes>
-                <Route path="/*" element={<App />} />
-            </Routes>
-        </Router>
-    </Provider>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </Router>
+        </Provider>
+    </React.StrictMode>,
 )
