ec608a249f4724c03b235571e82159676dcceeda
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-31 12:32:43 +0000

    Refactor routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import './index.css'
 
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { fetchUsers } from './features/users/usersSlice.js'
 import { store } from './app/store.js'
@@ -10,9 +11,11 @@ import { store } from './app/store.js'
 store.dispatch(fetchUsers())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
+    <Provider store={store}>
+        <Router>
+            <Routes>
+                <Route path="/*" element={<App />} />
+            </Routes>
+        </Router>
+    </Provider>,
 )
