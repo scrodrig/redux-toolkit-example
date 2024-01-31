fd0d309ed5de3158a22e92fb59bb2f8880b15a9a
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-31 15:42:41 +0000

    Add user-related routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import AddPostForm from './features/posts/AddPostForm'
 import EditPostForm from './features/posts/EditPostForm'
 import Layout from './components/Layout'
+import { Navigate } from 'react-router-dom'
 // import Counter from './features/counter/Counter' //* removed from lesson01
 import PostList from './features/posts/PostList'
 import SInglePostPage from './features/posts/SInglePostPage'
+import UserPage from './features/users/UserPage'
+import UsersList from './features/users/UsersList'
 
 function App() {
     return (
@@ -22,6 +25,12 @@ function App() {
                     <Route path=":postId" element={<SInglePostPage />} />
                     <Route path="edit/:postId" element={<EditPostForm />} />
                 </Route>
+                <Route path="user">
+                    <Route index element={<UsersList />} />
+                    <Route path=":userId" element={<UserPage />} />
+                </Route>
+                {/** Catach all not matching */}
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Route>
         </Routes>
     )
