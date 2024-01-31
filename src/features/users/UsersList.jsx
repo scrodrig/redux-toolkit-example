9618747d67e915a79ab1e47e627befa4ca39e414
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-31 15:42:24 +0000

    Add UsersList component

---
+++ src/features/users/UsersList.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+import { selectAllUsers } from './usersSlice'
+import { useSelector } from 'react-redux'
+
+const UsersList = () => {
+    const users = useSelector(selectAllUsers)
+
+    const renderedUsers = users.map(user => (
+        <li key={user.id}>
+            <Link to={`/user/${user.id}`}>{user.name}</Link>
+        </li>
+    ))
+
+    return (
+        <section>
+            <h2>Users</h2>
+
+            <ul>{renderedUsers}</ul>
+        </section>
+    )
+}
+
+export default UsersList
