e76f4d622dbfc7de3da5ad47dc79d31949736f32
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-31 12:33:02 +0000

    Add EditPostForm component

---
+++ src/features/posts/EditPostForm.jsx
@@ -0,0 +1,81 @@
+import { selectPostById, updatePost } from './postsSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate, useParams } from 'react-router-dom'
+
+import { selectAllUsers } from '../users/usersSlice'
+import { useState } from 'react'
+
+const EditPostForm = () => {
+    const { postId } = useParams()
+    const navigate = useNavigate()
+
+    const post = useSelector((state) => selectPostById(state, Number(postId)))
+    const users = useSelector(selectAllUsers)
+
+    const [title, setTitle] = useState(post?.title)
+    const [content, setContent] = useState(post?.body)
+    const [userId, setUserId] = useState(post?.userId)
+    const [requestStatus, setRequestStatus] = useState('idle')
+
+    const dispatch = useDispatch()
+
+    if (!post) {
+        return (
+            <section>
+                <h2>Post not found!</h2>
+            </section>
+        )
+    }
+
+    const onTitleChanged = (e) => setTitle(e.target.value)
+    const onContentChanged = (e) => setContent(e.target.value)
+    const onAuthorChanged = (e) => setUserId(Number(e.target.value))
+
+    const canSave = [title, content, userId].every(Boolean) && requestStatus === 'idle'
+
+    const onSavePostClicked = () => {
+        if (canSave) {
+            try {
+                setRequestStatus('pending')
+                dispatch(updatePost({ id: post.id, title, body: content, userId, reactions: post.reactions })).unwrap()
+
+                setTitle('')
+                setContent('')
+                setUserId('')
+                navigate(`/post/${postId}`)
+            } catch (err) {
+                console.error('Failed to save the post', err)
+            } finally {
+                setRequestStatus('idle')
+            }
+        }
+    }
+
+    const usersOptions = users.map((user) => (
+        <option key={user.id} value={user.id}>
+            {user.name}
+        </option>
+    ))
+
+    return (
+        <section>
+            <h2>Edit Post</h2>
+            <form>
+                <label htmlFor="postTitle">Post Title:</label>
+                <input type="text" id="postTitle" name="postTitle" value={title} onChange={onTitleChanged} />
+                <label htmlFor="postAuthor">Author:</label>
+                <select id="postAuthor" defaultValue={userId} onChange={onAuthorChanged}>
+                    <option value=""></option>
+                    {usersOptions}
+                </select>
+                <label htmlFor="postContent">Content:</label>
+                <textarea id="postContent" name="postContent" value={content} onChange={onContentChanged} />
+                <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+                    Save Post
+                </button>
+            </form>
+        </section>
+    )
+}
+
+export default EditPostForm
