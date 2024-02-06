49bee2f28029f8269ae212afd12a73e8d8345dbd
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-06 00:35:35 +0000

    Remove count feature from Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,6 @@
-import { getCount, increaseCount } from '../features/posts/postsSlice'
-import { useDispatch, useSelector } from 'react-redux'
-
 import { Link } from 'react-router-dom'
 
 const Header = () => {
-    const dispatch = useDispatch()
-    const count = useSelector(getCount)
-
     return (
         <header className="Header">
             <h1>Redux Blog</h1>
@@ -22,13 +16,6 @@ const Header = () => {
                         <Link to="user">Users</Link>
                     </li>
                 </ul>
-                <button
-                    onClick={() => {
-                        dispatch(increaseCount())
-                    }}
-                >
-                    {count}
-                </button>
             </nav>
         </header>
     )
