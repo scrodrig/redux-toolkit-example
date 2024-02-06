e9576d0eac8f5f9b74d0c3c11ed56b7ff1fde8a9
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-06 00:36:02 +0000

    Refactor UserPage component to use useGetPostsByUserIdQuery hook

--- src/features/users/UserPage.jsx
+++ src/features/users/UserPage.jsx
@@ -1,26 +1,34 @@
 import { Link, useParams } from 'react-router-dom'
-import { selectAllPosts, selectPostsByUser } from '../posts/postsSlice'
 
 import { selectUserById } from '../users/usersSlice'
+import { useGetPostsByUserIdQuery } from '../posts/postsSlice'
 import { useSelector } from 'react-redux'
 
 const UserPage = () => {
     const { userId } = useParams()
     const user = useSelector((state) => selectUserById(state, Number(userId)))
 
-    const postsForUser = useSelector((state) => selectPostsByUser(state, Number(userId)))
+    const { data: postsForUser, isLoading, isSuccess, isError, error } = useGetPostsByUserIdQuery(userId)
 
-    const postTitles = postsForUser.map((post) => (
-        <li key={post.id}>
-            <Link to={`/post/${post.id}`}>{post.title}</Link>
-        </li>
-    ))
+    let content
+    if (isLoading) {
+        content = <p>Loading...</p>
+    } else if (isSuccess) {
+        const { ids, entities } = postsForUser
+        content = ids.map((id) => (
+            <li key={id}>
+                <Link to={`/post/${id}`}>{entities[id].title}</Link>
+            </li>
+        ))
+    } else if (isError) {
+        content = <p>{error}</p>
+    }
 
     return (
         <section>
             <h2>{user?.name}</h2>
 
-            <ol>{postTitles}</ol>
+            <ol>{content}</ol>
         </section>
     )
 }
