775ed024913805b773d2d2702b8bff38f0a4c38f
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-06 00:35:31 +0000

    Update API call in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,12 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { fetchPosts } from './features/posts/postsSlice.js'
+import { extendedApiSlice } from './features/posts/postsSlice.js'
 import { fetchUsers } from './features/users/usersSlice.js'
 import { store } from './app/store.js'
 
 store.dispatch(fetchUsers())
-store.dispatch(fetchPosts())
+store.dispatch(extendedApiSlice.endpoints.getPosts.initiate())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
