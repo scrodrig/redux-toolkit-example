631d041d3f63593f4195bbb6868b324d19172416
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-06 00:35:58 +0000

    Refactor ReactionButtons component to use useAddReactionMutation hook

--- src/features/posts/ReactionButtons.jsx
+++ src/features/posts/ReactionButtons.jsx
@@ -1,35 +1,33 @@
 import PropTypes from 'prop-types'
-import { reactionAdded } from './postsSlice'
-import { useDispatch } from 'react-redux'
+import { useAddReactionMutation } from './postsSlice'
 
 const reactionEmoji = {
     thumbsUp: '👍',
-    wow: '😲',
+    wow: '😮',
     heart: '❤️',
     rocket: '🚀',
-    coffee: '☕️',
+    coffee: '☕',
 }
 
 const ReactionButtons = ({ post }) => {
-    const dispatch = useDispatch()
-    const reactionButtons = Object.entries(reactionEmoji).map(
-        ([name, emoji]) => {
-            return (
-                <button
-                    key={name}
-                    type="button"
-                    className="reactionButton"
-                    onClick={() =>
-                        dispatch(
-                            reactionAdded({ postId: post.id, reaction: name }),
-                        )
-                    }
-                >
-                    {emoji} {post.reactions[name]}
-                </button>
-            )
-        },
-    )
+    const [addReaction] = useAddReactionMutation()
+
+    const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+        return (
+            <button
+                key={name}
+                type="button"
+                className="reactionButton"
+                onClick={() => {
+                    const newValue = post.reactions[name] + 1
+                    addReaction({ postId: post.id, reactions: { ...post.reactions, [name]: newValue } })
+                }}
+            >
+                {emoji} {post.reactions[name]}
+            </button>
+        )
+    })
+
     return <div>{reactionButtons}</div>
 }
 
